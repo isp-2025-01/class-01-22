e1af429d0500080b9c4d34a0c9259d712d98336a
David Kane <dave.kane@gmail.com>
2025-01-23 11:31:51 -0500

    no longer need old data. new downloads

--- download.R
+++ download.R
@@ -1,15 +1,38 @@
 library(tidycensus)
 library(tidyverse)
-library(leaflet)
-library(sf)
 
+# Define the variables you want to extract
+education_variables <- c(
+  "B15003_022",  # Bachelor's degree
+  "B15003_023",  # Master's degree
+  "B15003_024",  # Professional school degree
+  "B15003_025",  # Doctorate degree
+  "B01003_001"   # Total population (for percentage calculations)
+)
+
+# Fetch the data
 fl_education_data <- get_acs(
   geography = "tract",  # Use "block group" for block group level
-  variables = "B15003_022",  # Example: Bachelor's degree or higher
+  variables = education_variables,  # Include all variables
   state = "FL",  # Florida
-  year = 2021,  # Specify the year
+  year = 2023,  # Specify the year
   survey = "acs5",  # 5-year ACS data
   geometry = TRUE  # Set to TRUE if you want spatial data
 )
 
-write_rds(fl_education_data, file = "data.rds")
+# Reshape the data to wide format for easier calculations
+fl_education_wide <- fl_education_data %>%
+  select(GEOID, NAME, variable, estimate) %>%
+  tidyr::pivot_wider(names_from = variable, values_from = estimate)
+
+# Calculate percentages for each education level
+fl_education_wide <- fl_education_wide %>%
+  mutate(
+    pct_bachelors = (B15003_022 / B01003_001) * 100,
+    pct_masters = (B15003_023 / B01003_001) * 100,
+    pct_professional = (B15003_024 / B01003_001) * 100,
+    pct_doctorate = (B15003_025 / B01003_001) * 100
+  )
+
+
+write_rds(fl_education_wide, file = "fl_data.rds")
